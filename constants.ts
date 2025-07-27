f4eab586c48cf2ac6be5643e1856b5abcd2f0dc0
ADICODZZ <aditya09sinha264@gmail.com>
2025-07-27 17:22:09 +0530

    minor icon changes

--- constants.ts
+++ constants.ts
@@ -71,7 +71,7 @@ export const SERIES_SEASON_COUNTS: string[] = [
 
 export const ICONS = {
   // Genres
-  action: `<img src="./icons/action.png" class="w-6 h-6 mr-1.5 inline-block"/>`,
+  action: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1.5 inline-block"><path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" /></svg>`,
   adventure: `<img src="./icons/hiking.png" class="w-6 h-6 mr-1.5 inline-block"/>`,
   drama:` <img src="./icons/theater.png" class="w-4 h-4 mr-1.5 inline-block"/>`,
   documentary: `<img src="./icons/documentary.png" class="w-4 h-4 mr-1.5 inline-block"/>`,
