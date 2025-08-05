31b34e3b5e80a114e48924ec5f9418772f26a96f
ADICODZZ <aditya09sinha264@gmail.com>
2025-08-06 01:54:33 +0530

    bug fixed

--- components/OnboardingWizard.tsx
+++ components/OnboardingWizard.tsx
@@ -221,7 +221,7 @@ export const OnboardingWizard: React.FC<OnboardingWizardProps> = ({ onComplete, 
               <img
             src="./icons/icon128.png"
             alt="CineMan AI logo"
-            className="w-20 h-20 text-purple-400 mx-auto mb-6
+            className="w-20 h-20 text-purple-400 mx-auto mb-6"
           />
             ) : (
              <img
